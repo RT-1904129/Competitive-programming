f9e32ed25f24864f9fda6bc40f8a0286e1405242
Rishabh Tripathi <74362927+RT-1904129@users.noreply.github.com>
2021-01-01 21:39:10 +0530

    Add files via upload

---
+++ Pangram problem.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+/* 
+Auther:Rishabh Tripathi
+Topic:-Pangram problem
+*/
+#include<string>
+#include<iomanip>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    int number,s,charracter_int,check;
+    cin>>number;
+    string t;
+    getline(cin, t);
+    for(int i=0;i<number;++i){
+        string r;
+        getline(cin, r);
+        s=r.length();
+        if(s<26){
+            cout<<"No\n";   //here we check total length of alphabate
+            continue;}
+        string rt;  //we will try to make a string which is always have no repited string
+        for(int i=0;i<s;++i){
+            charracter_int=r[i];
+            if ((charracter_int>=97 && charracter_int<=122)||(charracter_int>=65 && charracter_int<=90)){
+                check=rt.find(r[i]);
+                if(check==(-1)) rt+=r[i];}
+                }
+        //cout<<rt;
+        s=rt.length();
+        if(s<26){
+            cout<<"No\n";
+            continue;}
+        cout<<"Yes\n";
+    }
+}
