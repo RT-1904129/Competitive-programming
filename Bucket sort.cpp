2eaf8ae75c17efcae339749347e6e61250108da3
rt-1904129 <rishabh29072001@gmail.com>
2021-03-01 22:47:55 +0530

    its exact file

--- Bucket sort.cpp
+++ Bucket sort.cpp
@@ -1,50 +1,41 @@
-// C++ program to sort an 
-// array using bucket sort
-#include <algorithm>
 #include <iostream>
-#include<cmath>
+#include <algorithm>
 #include <vector>
 using namespace std;
-typedef long long int rt;
-// Function to sort arr[] of 
-// size n using bucket sort
-void bucketSort(float arr[], rt n,rt bucket_size){
-    
-	float maxi_element=*(max_element(arr,arr+n*sizeof(float)));
-	float mini_element=*(min_element(arr,arr+n*sizeof(float)));
-	float range=(maxi_element-mini_element)/bucket_size;
-	
-	// 1) Create n empty buckets
-	vector<float> b[bucket_size];
 
-	// 2) Put array elements 
-	// in different buckets
-	for (rt i = 0; i < n; i++) {
-		rt bi = (arr[i]-mini_element)/range; // Index in bucket
-		b[bi].push_back(arr[i]);
-	}
+void bucketSort(double arr[], int n)
+{
+    // add elements into buckets
+    vector<double> buckets[n];
+    for (int i = 0; i < n; i++)
+    {
+        int ind = n * arr[i] / 1000000000;
+        buckets[ind].push_back(arr[i]);
+    }
 
-	// 3) Sort individual buckets
-	for (rt i = 0; i < bucket_size; i++)
-		sort(b[i].begin(), b[i].end());
+    // sort buckets. STL sort uses insertion sort if elements to sort are few.
+    for (int i = 0; i < n; i++)
+        sort(buckets[i].begin(), buckets[i].end());
 
-	// 4) Concatenate all buckets into arr[]
-	rt index = 0;
-	for (rt i = 0; i < bucket_size; i++)
-		for (rt j = 0; j < b[i].size(); j++)
-			arr[index++] = b[i][j];
+    // concatenate all buckets
+    int index = -1;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < buckets[i].size(); j++)
+            arr[++index] = buckets[i][j];
+
+    return;
 }
 
-/* Driver program to test above function */
 int main()
 {
-	float arr[]= { 9.8 , 0.6 , 10.1 , 1.9 , 3.07 , 3.04 , 5.0 , 8.0 , 4.8 , 7.68 };
-	rt n = sizeof(arr) / sizeof(arr[0]);
-	
-	bucketSort(arr, n,5);
+    int N;
+    cin >> N;
+    double arr[N];
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    bucketSort(arr, N);
 
-	cout << "Sorted array is \n";
-	for (rt i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	return 0;
+    for (int i = 0; i < N; i++)
+        printf("%.2f ", arr[i]);
+    return 0;
 }
